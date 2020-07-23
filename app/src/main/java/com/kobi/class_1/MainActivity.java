ae9b5b9a45c4d186790e0b7cff4efb5fbd9e0128
guymacin <guyskv@gmail.com>
2020-07-23 19:23:50 +0300

    Initial commit

---
+++ app/src/main/java/com/kobi/class_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.kobi.class_1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
